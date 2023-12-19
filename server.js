4d944b41add32bbd9d073e81248ebf5101c83fcb
FelixJentsch <FelixJentsch91@googlemail.com>
2023-12-19 11:14:17 +0100

    challenge solved

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+const chance = new Chance();
+
+export const server = createServer((req, res) => {
+  const name = chance.name();
+  const age = chance.age({ type: "adult" });
+  const profession = chance.profession();
+
+  const response = `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`;
+
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(response);
+});
